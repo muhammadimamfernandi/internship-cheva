ea624a3474178d57ac92d80037a13ddc3397af9b
Muhammad Imam Fernandi <fernandi.bkw@gmail.com>
2021-01-15 23:13:10 +0800

    DOM Manipulation 2

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-// const judul = document.querySelector('#judul');
-// judul.innerHTML = '<em>Imam Fernandi</em>';
-// judul.style.textAlign = 'center';
+// DOM Manipulation
+// buat elemen
+const pBaru = document.createElement('p');
+const teksPBaru = document.createTextNode('paragraf baru');
 
-// const sectionA = document.getElementById('a');
-// sectionA.innerHTML = '<b>Hello World</b>';
-// sectionA.style.fontSize = '30px';
+//simpan tulisan ke tag paragraf
+pBaru.appendChild(teksPBaru);
 
-const judul = document.getElementsByTagName('h1')[0];
-const a = document.querySelector('section#a a');
-const p1 = document.getElementsByClassName('p1')[0];
+//simpan ke section a
+const sectionA = document.getElementById('a');
+sectionA.appendChild(pBaru);
+
+//buat elemen
+const liBaru = document.createElement('li');
+const textLiBaru = document.createTextNode('li baru')
+liBaru.appendChild(textLiBaru);
+
+const ul = document.querySelector('section#b ul');
+const li2 = ul.querySelector('li:nth-child(2)');
+
+ul.insertBefore(liBaru, li2);
+
+//menghapus child
+// section a sudah diambil di atas
+const a = sectionA.querySelector('a');
+sectionA.removeChild(a);
+
+const sectionB = document.getElementById('b');
+const p4 = sectionB.querySelector('p');
+
+const h2Baru = document.createElement('h2');
+const textH2Baru = document.createTextNode('h2 Baru');
+h2Baru.appendChild(textH2Baru);
+
+sectionB.replaceChild(h2Baru, p4);
+
+pBaru.style.backgroundColor = 'lightgreen';
+h2Baru.style.backgroundColor = 'lightgreen';
+liBaru.style.backgroundColor = 'lightgreen';
