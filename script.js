76e11747e0b37ebc0a407ac6d7d18ca653114e13
Muhammad Imam Fernandi <fernandi.bkw@gmail.com>
2021-01-15 14:00:15 +0800

    DOM Selector 2

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 // DOM Selection
-const judul = document.getElementById('judul');
-judul.style.color = 'red';
-judul.style.textAlign = 'center';
-judul.style.backgroundColor = '#ccc';
-judul.innerHTML = 'Halo Dunia';
+// const p4 = document.querySelector('#b p');
+// p4.style.color = 'green';
+// p4.style.fontSize = '30px';
 
-const p = document.getElementsByTagName('p');
+// const li2 = document.querySelector('section#b ul li:nth-child(2)');
+// li2.style.backgroundColor = 'orange';
 
-for (let i = 0; i < p.length; i++) {
-    p[i].style.backgroundColor = 'lightblue';  
-}
+// const p = document.querySelectorAll('p');
+// for (let i = 0; i < p.length; i++) {
+//     p[i].style.backgroundColor = 'lightblue';   
+// }
 
-const h1 = document.getElementsByTagName('h1')[0];
-h1.style.fontSize = '50px';
-
-const p1 = document.getElementsByClassName('p1')[0];
-p1.innerHTML = 'ini diubah dari javascript';
+const sectionB = document.getElementById('b');
+const p4 = sectionB.querySelector('p');
+p4.style.backgroundColor = 'orange'
