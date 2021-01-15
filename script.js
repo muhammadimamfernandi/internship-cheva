28ed428f6c88d38c453afaecace182aab957ad4c
Muhammad Imam Fernandi <fernandi.bkw@gmail.com>
2021-01-15 22:23:19 +0800

    DOM Manipulation 1

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-// DOM Selection
-// const p4 = document.querySelector('#b p');
-// p4.style.color = 'green';
-// p4.style.fontSize = '30px';
+// const judul = document.querySelector('#judul');
+// judul.innerHTML = '<em>Imam Fernandi</em>';
+// judul.style.textAlign = 'center';
 
-// const li2 = document.querySelector('section#b ul li:nth-child(2)');
-// li2.style.backgroundColor = 'orange';
+// const sectionA = document.getElementById('a');
+// sectionA.innerHTML = '<b>Hello World</b>';
+// sectionA.style.fontSize = '30px';
 
-// const p = document.querySelectorAll('p');
-// for (let i = 0; i < p.length; i++) {
-//     p[i].style.backgroundColor = 'lightblue';   
-// }
-
-const sectionB = document.getElementById('b');
-const p4 = sectionB.querySelector('p');
-p4.style.backgroundColor = 'orange'
+const judul = document.getElementsByTagName('h1')[0];
+const a = document.querySelector('section#a a');
+const p1 = document.getElementsByClassName('p1')[0];
