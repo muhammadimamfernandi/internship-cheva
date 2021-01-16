5498735b39a9f27e3bf08a83cfe3a4c753ee2067
Muhammad Imam Fernandi <fernandi.bkw@gmail.com>
2021-01-16 17:51:31 +0800

    Prevent Default

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ for (let index = 0; index < close.length; index++) {
     close[index].addEventListener('click', function (event) {
         // close[index].parentElement.style.display = 'none';
         event.target.parentElement.style.display = 'none';
+        event.preventDefault();
     })
-    
 }
 
-const nama = document.querySelector('.nama');
+// const nama = document.querySelector('.nama');
 
-console.log(nama.parentElement);
-console.log(nama.nextElementSibling);
-console.log(nama.previousElementSibling);
+// console.log(nama.parentElement);
+// console.log(nama.nextElementSibling);
+// console.log(nama.previousElementSibling);
