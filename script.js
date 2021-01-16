b751ad3810afd676f81f132ea30b22000c3a9e66
Muhammad Imam Fernandi <fernandi.bkw@gmail.com>
2021-01-16 17:13:00 +0800

    DOM Traversal

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
-const p1 = document.querySelector('.p1');
+// const card = document.querySelector('.card');
+// const close = document.querySelector('.close');
 
-function ubahWarnaP1(){
-    p1.style.backgroundColor = 'lightblue';
+// close.addEventListener('click', function (){
+//     card.style.display = 'none';
+// })
+
+const close = document.querySelectorAll('.close');
+
+for (let index = 0; index < close.length; index++) {
+    close[index].addEventListener('click', function (event) {
+        // close[index].parentElement.style.display = 'none';
+        event.target.parentElement.style.display = 'none';
+    })
+    
 }
 
-const p3 = document.querySelector('.p3');
-function ubahWarnaP2() {
-    p3.style.backgroundColor = 'lightgreen';
-}
-p3.onclick = ubahWarnaP2;
+const nama = document.querySelector('.nama');
 
-const p4 = document.querySelector('section#b p');
-p4.addEventListener('click', function () {
-    const ul = document.querySelector('section#b ul');
-    const liBaru = document.createElement('li');
-    const teksLiBaru = document.createTextNode('li baru');
-
-    liBaru.appendChild(teksLiBaru);
-    ul.appendChild(liBaru);
-})
-
-const p2 = document.querySelector('.p2');
-p2.addEventListener('mouseenter', function () {
-    p2.style.backgroundColor = 'pink';
-})
-p2.addEventListener('mouseleave', function (){
-    p2.style.backgroundColor = 'white';
-})
+console.log(nama.parentElement);
+console.log(nama.nextElementSibling);
+console.log(nama.previousElementSibling);
