062f5985752ec02f6f2fb0d962f0a4189a6b5fe8
Muhammad Imam Fernandi <fernandi.bkw@gmail.com>
2021-01-16 15:16:22 +0800

    DOM Events

--- script.js
+++ script.js
@@ -1,39 +1,29 @@
-// DOM Manipulation
-// buat elemen
-const pBaru = document.createElement('p');
-const teksPBaru = document.createTextNode('paragraf baru');
+const p1 = document.querySelector('.p1');
 
-//simpan tulisan ke tag paragraf
-pBaru.appendChild(teksPBaru);
+function ubahWarnaP1(){
+    p1.style.backgroundColor = 'lightblue';
+}
 
-//simpan ke section a
-const sectionA = document.getElementById('a');
-sectionA.appendChild(pBaru);
+const p3 = document.querySelector('.p3');
+function ubahWarnaP2() {
+    p3.style.backgroundColor = 'lightgreen';
+}
+p3.onclick = ubahWarnaP2;
 
-//buat elemen
-const liBaru = document.createElement('li');
-const textLiBaru = document.createTextNode('li baru')
-liBaru.appendChild(textLiBaru);
+const p4 = document.querySelector('section#b p');
+p4.addEventListener('click', function () {
+    const ul = document.querySelector('section#b ul');
+    const liBaru = document.createElement('li');
+    const teksLiBaru = document.createTextNode('li baru');
 
-const ul = document.querySelector('section#b ul');
-const li2 = ul.querySelector('li:nth-child(2)');
+    liBaru.appendChild(teksLiBaru);
+    ul.appendChild(liBaru);
+})
 
-ul.insertBefore(liBaru, li2);
-
-//menghapus child
-// section a sudah diambil di atas
-const a = sectionA.querySelector('a');
-sectionA.removeChild(a);
-
-const sectionB = document.getElementById('b');
-const p4 = sectionB.querySelector('p');
-
-const h2Baru = document.createElement('h2');
-const textH2Baru = document.createTextNode('h2 Baru');
-h2Baru.appendChild(textH2Baru);
-
-sectionB.replaceChild(h2Baru, p4);
-
-pBaru.style.backgroundColor = 'lightgreen';
-h2Baru.style.backgroundColor = 'lightgreen';
-liBaru.style.backgroundColor = 'lightgreen';
+const p2 = document.querySelector('.p2');
+p2.addEventListener('mouseenter', function () {
+    p2.style.backgroundColor = 'pink';
+})
+p2.addEventListener('mouseleave', function (){
+    p2.style.backgroundColor = 'white';
+})
